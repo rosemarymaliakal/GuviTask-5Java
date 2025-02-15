146cd3badb18926eaf22a6239041febe6f16b96c
rosemarymaliakal <47358144+rosemarymaliakal@users.noreply.github.com>
2025-02-16 02:00:35 +0530

    Add files via upload

---
+++ GradingSystemTask5.java
@@ -0,0 +1,47 @@
+package GuviJavaTask2;
+import java.util.Scanner;
+
+public class GradingSystemTask5 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enetr the student mark");
+		
+		int mark = scanner.nextInt();
+		
+//		System.out.println("Mark is "+mark);
+		
+		if (mark >100) {
+			System.out.println("Invalid Input");
+		}
+		else if (mark == 100) {
+			System.out.println("S");
+			
+		}
+		else if (mark > 90 && mark<99) {
+			System.out.println("A");
+			
+		}
+		else if (mark > 80 && mark<89) {
+			System.out.println("B");
+			
+		}
+		else if (mark > 70 && mark<79) {
+			System.out.println("C");
+			
+		}
+		else if (mark > 60 && mark<69) {
+			System.out.println("D");
+			
+		}
+		else if (mark > 50 && mark<59) {
+			System.out.println("E");
+			
+		}
+		else if (mark<50) {
+			System.out.println("F");
+			
+		}
+	}
+
+}
